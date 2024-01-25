ea00b94528e9b635d910c74c8000ccbc25bfb8f1
XinyuanMO <157107733+XinyuanMO@users.noreply.github.com>
2024-01-25 20:22:07 +0200

    Update WEEK_1_web_text_mining.py
    Now this py file also extracts the first 10 pies (their names & corresponding descriptions) 
    
    :)

--- WEEK_1_web_text_mining.py
+++ WEEK_1_web_text_mining.py
@@ -1,30 +1,30 @@
-<<<<<<< HEAD
 from bs4 import BeautifulSoup
 import requests
 
+# retreat pie info from BBC goodfood
 url = "https://www.bbcgoodfood.com/recipes/collection/pie-recipes"
-
 result = requests.get(url)
+soup = BeautifulSoup(page.text, 'html')
 
-soup = BeautifulSoup(result.text, "html.parser")
+# EXTRACT PAGE INTRO
+intro = soup.find("p").text
 
-result = soup.find("p").text
+#EXTRACT PIE NAMES (the first 10 of them)
+all_pie_names = soup.find_all('h2', class_ = 'heading-4')
+ten_pies = [pie.text for pie in all_pie_names[0:10]]
 
-print(result)
-=======
-import nltk, re, pprint
-from nltk import word_tokenize
-from urllib import request
-from bs4 import BeautifulSoup
+#EXTRACT CORRESPONDING PIE DESCRIPTIONS
+pie_descriptions = soup.find_all('p', class_ = 'card__description d-block body-copy-small')
+ten_descriptions = [des.text for des in pie_descriptions[0:10]]
 
-# retreat info from Sally's Baking Recipes (Pies, Crisps, & Tarts)
-url = "https://sallysbakingaddiction.com/category/desserts/pies-crisps-tarts/"
-html = request.urlopen(url).read().decode('utf8')
+#print everything:
+print("# From a BBC good food webpage, we extracted a general introduction quote and information for 10 pies on that page :)\n")
 
-# pre-processed content
-raw = raw = BeautifulSoup(html, 'html.parser').get_text()
+print("Webpage Intro:")
+print(intro)
 
-# tokenize raw data
-tokens = word_tokenize(raw)
-text = nltk.Text(tokens)
->>>>>>> d155fd72883bd4dacb0fcf40106f7e46c9f68ed4
+for idx in range(10):
+    name = ten_pies[idx]
+    desc = ten_descriptions[idx]
+    print("Pie #",idx+1,"- "+name)
+    print(desc+".\n")
