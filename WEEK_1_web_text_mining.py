0bf503c35791e37a5e6be6be4d944dc856bc9b03
XinyuanMO <157107733+XinyuanMO@users.noreply.github.com>
2024-01-25 14:53:24 +0200

    Update WEEK_1_web_text_mining.py

--- WEEK_1_web_text_mining.py
+++ WEEK_1_web_text_mining.py
@@ -1,4 +1,13 @@
 import nltk, re, pprint
 from nltk import word_tokenize
 from urllib import request
-url = ""
+
+# retreat info from Sally's Baking Recipes (Pies, Crisps, & Tarts)
+url = "https://sallysbakingaddiction.com/category/desserts/pies-crisps-tarts/"
+response = request.urlopen(url)
+
+# pre-processed content
+raw = response.read().decode('utf8')
+
+# tokenie raw data
+tokens = word_tokenize(raw)
