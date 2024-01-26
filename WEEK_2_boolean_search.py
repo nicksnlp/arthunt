72e9e3854d1ae29c9e8a243e2f7e7500e7433992
Zongchan Chen <chenzong@puhti-login15.bullx>
2024-01-26 13:17:17 +0200

    I copied some codes from the notebook

--- WEEK_2_boolean_search.py
+++ WEEK_2_boolean_search.py
@@ -1,1 +1,13 @@
+documents = [""]
 
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer(lowercase=True, binary=True)
+sparse_matrix = cv.fit_transform(documents)
+
+dense_matrix = sparse_matrix.todense()
+td_matrix = dense_matrix.T   # .T transposes the matrix
+print(cv.get_feature_names_out())
+
+terms = cv.get_feature_names_out()
+print(cv.vocabulary_) # note the _ at the end
