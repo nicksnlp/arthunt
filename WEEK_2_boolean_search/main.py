f3c0e8b69be24386659c1cd385216a715fe0fdd5
Nikolay Vorontsov <nikolay.vorontsov@gmail.com>
2024-01-26 13:52:35 +0200

    #1 added main file to gather functions

---
+++ WEEK_2_boolean_search/main.py
@@ -0,0 +1,16 @@
+# This the main file for NLP app project. Spring, 2024. Helsinki University.
+
+# Import functions here
+
+# from xinyuan.py import ...
+# from nicksnlp.py import ...
+# from zccwqdoorchid.py import ...
+
+def main():
+
+
+# Build something here
+
+
+
+main()
