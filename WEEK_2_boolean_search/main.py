d72198536c9041dd12bb940461ffa245fd7f8b2f
XinyuanMO <157107733+XinyuanMO@users.noreply.github.com>
2024-01-30 20:25:10 +0200

    Update main.py
    Add: codes for reading contents from txt file (the 100 wiki articles) and convert it into a list of strings.
    
    (Note: the article names are with the content in a single string)

--- WEEK_2_boolean_search/main.py
+++ WEEK_2_boolean_search/main.py
@@ -1,17 +1,9 @@
 # This the main file for NLP app project. Spring, 2024. Helsinki University.
 from sklearn.feature_extraction.text import CountVectorizer
+import nick, zongchan, xinyuan # Import functions from all members' works
 
-# Import functions here
-
-# from xinyuan.py import ...
-# from nick.py import ...
-# from zongchan.py import ...
-
-def main():
-
-
-  # Build something here
-
-
-
-main()
+# read from 100 wiki articles
+with open('enwiki-20181001-corpus.100-articles.txt', 'r', encoding='UTF-8') as f:
+    file_chunk = f.read().replace('\n', ' ')    # replace all newline characters with single space
+    documents = file_chunk.split('</article>')  # split the file(str) into list
+    del documents[-1]   # remove the last element, which is empty (which was caused by an </article> in the END of the document)
