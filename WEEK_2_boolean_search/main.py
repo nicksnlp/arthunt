4090eb1571888a61ea01a24a83628a3b5caff9f4
XinyuanMO <157107733+XinyuanMO@users.noreply.github.com>
2024-02-01 10:45:15 +0200

    Update main.py
    debug a spelling mistake

--- WEEK_2_boolean_search/main.py
+++ WEEK_2_boolean_search/main.py
@@ -19,7 +19,7 @@ query = input("Your input here: ").lower() #convert input to lower case
 # keep asking for an input query until the user quits
 while query != "  ":
     if_exist = zongchan.invalid_term(query, td_matrix, terms, t2i)
-    if if_exist == false : # query word does not exist
+    if if_exist == False: # query word does not exist
         print(f"Sorry, the token {@@@ZHONGCHAN func outputs} is not among our word list. You may chose to:")
         print("1. Start a new search -- type your query and press enter")
         print("2. QUIT -- enter 2 white spaces and press enter\n")
