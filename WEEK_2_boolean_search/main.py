a24eb13164ffa0c98d7a357ec1c047ec65855dd4
XinyuanMO <157107733+XinyuanMO@users.noreply.github.com>
2024-02-01 10:40:44 +0200

    Update main.py
    added query-asking loop

--- WEEK_2_boolean_search/main.py
+++ WEEK_2_boolean_search/main.py
@@ -3,7 +3,35 @@ from sklearn.feature_extraction.text import CountVectorizer
 import nick, zongchan, xinyuan # Import functions from all members' works
 
 # read from 100 wiki articles
-with open('enwiki-20181001-corpus.100-articles.txt', 'r', encoding='UTF-8') as f:
-    file_chunk = f.read().replace('\n', ' ')    # replace all newline characters with single space
-    documents = file_chunk.split('</article>')  # split the file(str) into list
-    del documents[-1]   # remove the last element, which is empty (which was caused by an </article> in the END of the document)
+documents = zongchan.read_file('enwiki-20181001-corpus.100-articles.txt')
+
+#generate term matrix
+td_matrix, terms, t2i = zongchan.term_matrix(documents)
+
+# opening message, shows up only once from the beginning 
+print("Welcome! You can search for articels by entering a query using the keyboard!")
+print("-- To search for articles, please type your query, then press enter;")
+print("-- To QUIT searching, please enter 2 white spaces, then press enter.\n")
+
+# user's first input
+query = input("Your input here: ").lower() #convert input to lower case
+
+# keep asking for an input query until the user quits
+while query != "  ":
+    if_exist = zongchan.invalid_term(query, td_matrix, terms, t2i)
+    if if_exist == false : # query word does not exist
+        print(f"Sorry, the token {@@@ZHONGCHAN func outputs} is not among our word list. You may chose to:")
+        print("1. Start a new search -- type your query and press enter")
+        print("2. QUIT -- enter 2 white spaces and press enter\n")
+        query = input("Your new input here: ").lower()
+    else:
+        print("Query recieved. We've found you these articles:")
+        # a query recieved! go fetch the articles!
+        # [NICK'S FUNCTION HERE]
+        print("The search has completed. You may chose to:")
+        print("1. Start a new search -- type your query and press enter")
+        print("2. QUIT -- enter 2 white spaces and press enter\n")
+        query = input("Your new input here: ").lower()
+
+# user choose QUIT, ending the while loop
+print("Got it. See you next time!")
