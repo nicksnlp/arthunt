3256423ab049d3e4cba45d0bbf47db982b179535
Zongchan Chen <chenzong@puhti-login15.bullx>
2024-02-02 13:12:48 +0200

    fixed code with updated version of sklearn

--- WEEK_2_boolean_search/zongchan.py
+++ WEEK_2_boolean_search/zongchan.py
@@ -12,7 +12,7 @@ def term_matrix(documents):
     sparse_matrix = cv.fit_transform(documents)
     dense_matrix = sparse_matrix.todense()
     td_matrix = dense_matrix.T
-    terms = cv.get_feature_names()
+    terms = cv.get_feature_names_out()
     t2i = cv.vocabulary_
     return td_matrix, terms, t2i
 
@@ -22,12 +22,10 @@ def invalid_term(query, td_matrix, terms, t2i):
         index = t2i[query]
         column_sum = td_matrix[index].sum()
         print(f"The term '{query}' found in article {column_sum}.")
-        return True
     else:
         print(f"Invalid term: '{query}'. Please enter a valid word.")
-        return False
 
 
 file_name = 'enwiki-20181001-corpus.100-articles.txt'
 documents = read_file(file_name)
-td_matrix, terms, t2i = term_matrix(documents) ## what is it? Nick
+td_matrix, terms, t2i = term_matrix(documents)
