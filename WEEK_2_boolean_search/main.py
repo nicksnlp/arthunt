a697415bc996c23e3e77660c90879df3ef59441d
Zongchan Chen <chenzong@puhti-login14.bullx>
2024-02-04 14:12:56 +0200

    all the codes are now in the main file

--- WEEK_2_boolean_search/main.py
+++ WEEK_2_boolean_search/main.py
@@ -1,12 +1,23 @@
 # This the main file for NLP app project. Spring, 2024. Helsinki University.
 from sklearn.feature_extraction.text import CountVectorizer
-import zongchan # Import functions from all members' works
 
 # read from 100 wiki articles
-documents = zongchan.read_file('enwiki-20181001-corpus.100-articles.txt')
+def read_file(file_name):
+    with open(file_name, 'r', encoding='UTF-8') as f:
+        file_chunk = f.read().replace('\n', ' ')
+        documents = file_chunk.split('</article>')
+        del documents[-1]
+    return documents
 
 #generate term matrix
-td_matrix, terms, t2i = zongchan.term_matrix(documents)
+def term_matrix(documents):
+    cv = CountVectorizer()
+    sparse_matrix = cv.fit_transform(documents)
+    dense_matrix = sparse_matrix.todense()
+    td_matrix = dense_matrix.T
+    terms = cv.get_feature_names_out()
+    t2i = cv.vocabulary_
+    return td_matrix, terms, t2i
 
 d = {"and": "&", "AND": "&",
      "or": "|", "OR": "|",
@@ -14,7 +25,7 @@ d = {"and": "&", "AND": "&",
      "(": "(", ")": ")"}          # operator replacements
 
 def rewrite_token(t):
-    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) 
+    return d.get(t, 'td_matrix[t2i.get("{:s}", 0)]'.format(t))
 
 def rewrite_query(query): # rewrite every token in the query
     return " ".join(rewrite_token(t) for t in query.split())
@@ -31,7 +42,21 @@ def print_contents(query, documents):
         print("Matching doc #{:d}  ".format(i))
         print(documents[doc_idx][:300])
         print()
-    
+
+def invalid_term(query, td_matrix, terms, t2i):
+    if query in terms: # query = user input
+        index = t2i[query]
+        column_sum = td_matrix[index].sum()
+        print(f"The term '{query}' found in article {column_sum}.")
+        return True
+    else:
+        print(f"Invalid term: '{query}'. Please enter a valid word.")
+        return False
+
+file_name = 'enwiki-20181001-corpus.100-articles.txt'
+documents = read_file(file_name)
+td_matrix, terms, t2i = term_matrix(documents)
+
 def main():
     #--------------------------------------------------------------------------------------
     # opening message, shows up only once from the beginning 
@@ -44,7 +69,7 @@ def main():
     #--------------------------------------------------------------------------------------
     # keep asking for an input query until the user quits
     while query != "  ":
-        if_exist = zongchan.invalid_term(query, td_matrix, terms, t2i)
+        if_exist = invalid_term(query, td_matrix, terms, t2i)
         if if_exist == False: # query word does not exist
             print(f"Sorry, the query '{query}' contain word(s) that is not in our word list. You may chose to:")
             print("1. Start a new search -- type your query and press enter")
