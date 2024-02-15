e84c7e9db0fad8a728518aec613a2789e7c3f52a
XinyuanMO <157107733+XinyuanMO@users.noreply.github.com>
2024-02-15 16:34:25 +0200

    Update search_articles.py

--- WEEK_4_web_UI/search_articles.py
+++ WEEK_4_web_UI/search_articles.py
@@ -1,1 +1,6 @@
-
+def read_file(file_name):
+    with open(file_name, 'r', encoding='UTF-8') as f:
+        file_chunk = f.read().replace('\n', ' ')    # replace all newline characters with single space
+        documents = file_chunk.split('</article>')  # split the file(str) into list
+        del documents[-1]                           # remove the last element, which is empty (which was caused by an </article> in the END of the document)
+    return documents    # type: list of strings
