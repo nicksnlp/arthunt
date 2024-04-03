460e030d69963353cb72f1008c652c9b568b74ab
nicksnlp <nick@Nikolays-Laptop.local>
2024-04-03 13:03:10 +0300

    clearing old plot files  on restart/scraping implemented

--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,19 @@ from flask import Flask, render_template, request
 from gallery_search import GallerySearch
 import os
 
+def clear_data():
+    # remove the plot after it has been passed to render:
+    for filename in os.listdir('static/'):
+        if filename.startswith('query_') and filename.endswith('_plot.png'):
+            filepath = os.path.join('static/', filename)
+            os.remove(filepath)
+
+#------
+
 app = Flask(__name__)
 gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
+
+clear_data()
 
 @app.route('/')
 def welcoming_message():
@@ -11,7 +22,7 @@ def welcoming_message():
 
 @app.route('/search')
 def search():
-
+        
     query = request.args.get('query')
     search_data = gallery_search.search(query)
     return render_template('index_combined.html', **search_data)
@@ -30,9 +41,7 @@ def scrape():
         scrape_message = "Old JSON file not found. Scraping restarted."
         gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
 
+    clear_data()
     return render_template('index_combined.html', scrape_message=scrape_message)
 
-    
-    scrape_message = "JSON file removed and Flask restarted."
-    return render_template('index_combined.html', scrape_message=scrape_message)
 ### CHATGPT ends, thinking never ends...
