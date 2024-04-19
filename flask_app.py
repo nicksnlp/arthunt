77fd5e0f28da5b70564ff28b492591a0bdd9999b
nicksnlp <nick@Nikolays-Laptop.local>
2024-04-19 15:20:30 +0300

    exceptions to flask_app, use back-up data if scraping fails, set a back-up data on startup, if no data is left from previous sessions, ie. previous sessions failed.

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from gallery_search import GallerySearch
-import os
+import os, shutil
 
 def clear_data():
     # remove old plots:
@@ -12,6 +12,13 @@ def clear_data():
 #------
 
 app = Flask(__name__)
+data_file_path = './static/scraped_data.json'
+
+if os.path.exists(data_file_path):
+    pass
+else:
+    shutil.copy('./back_up_json/scraped_data.json', './static/scraped_data.json') # COPY DATA from a back up, USE SHUTIL both for linux/mac and win
+        
 gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
 
 clear_data()
@@ -31,16 +38,27 @@ def search():
 @app.route('/scrape', methods=['POST'])
 def scrape():
 
-    data_file_path = './static/scraped_data.json'
     if os.path.exists(data_file_path):
-        os.remove(data_file_path)
-        scrape_message = "Old JSON file removed. Scraping restarted."
-        gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
-        
-    else:
-        scrape_message = "Old JSON file not found. Scraping restarted."
-        gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
+        try: #try scraping the file
+            os.remove(data_file_path) #need to remove old data, in order to initiate the new scraping (in gallery_search.extract_gallery_info / web_scraping.extract_gallery_info)
+            gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
+            scrape_message = "Old JSON file removed. Scraping restarted. New file created."
+        except: #if scraping failed (for eny reason, e.g. corrupted website), use back_up file
+            shutil.copy('./back_up_json/scraped_data.json', './static/scraped_data.json') # USE SHUTIL both for linux/mac and win
+            gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'}) # it uses data from a file, otherwise scrapes
+            scrape_message = "Old JSON file removed. Scraping failed. Back-up data is used."
 
+        
+    else: #.json file is missing for any reason (for example the process was interrupted earlier)
+        try: #try scraping the web        
+            gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'})
+            scrape_message = "Old JSON file not found. Scraping restarted. New file created."
+        except: #if scraping failed (for eny reason, e.g. corrupted website), use back_up file
+            shutil.copy('./back_up_json/scraped_data.json', './static/scraped_data.json') # USE SHUTIL both for linux/mac and win
+            gallery_search = GallerySearch({'Tate': 'https://www.tate.org.uk/whats-on'}) # it uses data from a file, otherwise scrapes
+            scrape_message = "Old JSON file not found. Scraping failed. Back-up data is used."
+
+            
     clear_data()
     return render_template('index_combined.html', scrape_message=scrape_message)
 
